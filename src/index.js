612a76dea2e6fd32807246d4c87ec76d8d152107
Amanda Wong <awong10@berkeley.edu>
2026-06-03 14:54:54 -0700

    initial proj setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const authRoutes = require('./routes/auth')
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(cors())
+app.use(express.json())
+
+app.use(' /api/auth', authRoutes)
+
+app.get('/', (req, res) => res.json({ status: 'SetLog API running' }))
+
+app.listen(PORT, () => console.log(`Server on port ${PORT}`))
